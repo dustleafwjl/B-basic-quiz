54e18d6c305909fc47b836602ec4a57d1af2dfb9
dustleafwjl <wangjianglin_priv@qq.com>
2020-09-15 11:04:56 +0800

    [jianglin] feat: init project

---
+++ src/main/java/com/thoughtworks/gtb/basicquiz/BasicquizApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.gtb.basicquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicquizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thoughtworks/gtb/basicquiz/BasicquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.gtb.basicquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
