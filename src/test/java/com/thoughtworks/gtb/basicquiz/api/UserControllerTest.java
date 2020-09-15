30e301091b7d6fc1289be8f6759ad9e8ff4823b0
dustleafwjl <wangjianglin_priv@qq.com>
2020-09-15 12:34:26 +0800

    [jianglin] feat: add skeleton

---
+++ src/test/java/com/thoughtworks/gtb/basicquiz/api/UserControllerTest.java
@@ -0,0 +1,28 @@
+package com.thoughtworks.gtb.basicquiz.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void should_return_user_info_when_get_users_givent_id() throws Exception {
+        mockMvc.perform(get("/users/1"))
+                .andExpect(jsonPath("$.name", is("test")))
+                .andExpect(status().isOk());
+    }
+}
